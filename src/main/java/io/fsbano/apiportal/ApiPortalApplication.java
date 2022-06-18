295eb12481314f9f9c69f39a90ec93b813fb9a7a
Fabio Sbano <fsbano@yahoo.com>
2022-06-18 12:36:32 -0300

    Initial project import

---
+++ src/main/java/io/fsbano/apiportal/ApiPortalApplication.java
@@ -0,0 +1,13 @@
+package io.fsbano.apiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPortalApplication.class, args);
+	}
+
+}
